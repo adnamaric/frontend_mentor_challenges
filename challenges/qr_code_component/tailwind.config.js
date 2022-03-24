7371504f5a9ce0248b3f031816d8fb9d64555692
adnamaric <adna.maric@edu.fit.ba>
2022-03-24 16:01:38 +0100

    Revert "QR code changes"
    This reverts commit ee0d08971243413b721514906f4851b0065877cf, reversing
    changes made to 2c9b0a63ffdf4878376a36562040056c25dcf5e7.

--- challenges/qr_code_component/tailwind.config.js
+++ challenges/qr_code_component/tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
       colors: {
         'optimum-blue': '#009efc',
         'optimum-darkblue': '#0389e1',
-        'light-gray': '#D6E2F0',
-        'dark-gray': '#7B879D',
-        'dark-blue': '#1f3251',
+        lightgray: '#D6E2F0',
+        grayishgray: '#7B879D',
+        darkblue: '#1f3251',
         primary: '#FF6363',
         secondary: {
           100: '#E2E2D5',
@@ -17,9 +17,11 @@ module.exports = {
           Outfit: ['Outfit'],
         },
         screens: {
-          desktop: '1200px',
-          mobile: '370px',
+          sm: '375px',
+          // => @media (min-width: 640px) { ... }
+
           lg: '1440px',
+          // => @media (min-width: 1024px) { ... }
         },
       },
     },
